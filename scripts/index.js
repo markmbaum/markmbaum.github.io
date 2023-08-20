e1b506d78eee4d0b93c2c42b04477c831a4bb6b4
Mark Baum <>
2023-08-20 07:20:37 -0700

    add fast forward buttons, reorganize and comment scripts

--- scripts/index.js
+++ scripts/index.js
@@ -1,94 +1,10 @@
-function randomInterval (t) {
-    return( t*2*Math.random() );
-}
-
-function bernoulli (p) {
-    return( (Math.random() < p) ? true : false );
-}
-
-function selectTime(char) {
-    t = 0;
-    if (char == "\n") {
-        t += randomInterval(100);
-        if (bernoulli(0.02)) t += 1000;
-    } else if (char == " ") {
-        t += randomInterval(50);
-        if (bernoulli(0.02)) t += 250;
-    } else {
-        t += randomInterval(15);
-    }
-    return(t);
-}
-
-function highlightPython (s) {
-    return hljs.highlight(s, {language: "python"}).value;
-}
-
-function highlightJulia (s) {
-    return hljs.highlight(s, {language: "julia"}).value;
-}
-
-function typeWriter (target, content, highlightFunc, finalFunc, index = 0) {
-    if (index <= content.length) {
-        var char = content.charAt(index);
-        if (char == ' ' || char == '\n') {
-            target.innerHTML = highlightFunc(content.slice(0,index+1));
-        } else {
-            target.innerHTML += content.charAt(index);
-        }
-        
-        setTimeout(
-            typeWriter,
-            selectTime(char),
-            target,
-            content,
-            highlightFunc,
-            finalFunc,
-            index + 1,
-        );
-    } else {
-        target.innerHTML = highlightFunc(content);
-        finalFunc();
-    }
-}
-
-function stripExtension(element) {
-    html = element.innerHTML;
-    idx = html.indexOf('.');
-    element.innerHTML = html.slice(0,idx) + '/';
-}
-
-function scriptColumns (button, script, right, left, text, highlighter, label, endFunc=function(){}) {
-    button.one("click", function () {
-        typeWriter(
-            script,
-            text,
-            highlighter,
-            function () {
-                setTimeout(
-                    function () { right.slideDown(500) },
-                    250
-                );
-                setTimeout(
-                    function () { stripExtension(label.children[0]) },
-                    250
-                );
-                setTimeout(
-                    function () { left.hide(500); },
-                    10000
-                );
-                setTimeout(
-                    function () { right.animate({width: "100%"}, 500) },
-                    10000
-                );
-                endFunc();
-            }
-        );
-    });
-}
-
+// right columns of the code animation blocks are initially hidden
 $(".right-column").hide(0);
 
+// fast forward buttons in the typewriter elements are dimmed to zero, but *not* hidden
+$(".fast-forward").css("opacity", "0");
+
+// cards in the accordion blocks glow a bit on hover
 $(".card").hover(
     function () {
         $(this).find(".card-text").css("text-decoration", "underline");
@@ -98,6 +14,7 @@ $(".card").hover(
     }
 );
 
+// add the typewriter animation sequence to the "research" block
 scriptColumns(
     $("#research-button"),
     document.getElementById("research-script"),
@@ -105,9 +22,11 @@ scriptColumns(
     $("#research-left"),
     research_text,
     highlightPython,
-    document.getElementById('research-button')
+    document.getElementById('research-button'),
+    $("#research-fast-forward")
 )
 
+// add the typewriter animation sequence to the "edu" block
 scriptColumns(
     $("#edu-button"),
     document.getElementById("edu-script"),
@@ -116,6 +35,7 @@ scriptColumns(
     edu_text,
     highlightJulia,
     document.getElementById('edu-button'),
+    $("#edu-fast-forward"),
     function () {
         setTimeout(
             function () {$("#advisor-error").hide(500)},
