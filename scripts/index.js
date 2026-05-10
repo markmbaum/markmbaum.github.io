95053280abe3025d035ca09524a7b6a74ac1f91d
Mark Baum <markmbaum@protonmail.com>
2026-05-09 21:36:47 -0700

    Small updates (#7)
    * format js
    
    * small updates

--- scripts/index.js
+++ scripts/index.js
@@ -12,34 +12,33 @@ $(".card").hover(
     function () {
         $(this).find(".card-text").css("text-decoration", "none");
     }
-    );
+);
 
-    // add the typewriter animation sequence to the "research" block
-    scriptColumns(
-        $("#research-button"),
-        document.getElementById("research-script"),
-        $("#research-right"),
-        $("#research-left"),
-        research_text,
-        highlightPython,
-        document.getElementById('research-button'),
-        $("#research-fast-forward")
-        )
+// add the typewriter animation sequence to the "research" block
+scriptColumns(
+    $("#research-button"),
+    document.getElementById("research-script"),
+    $("#research-right"),
+    $("#research-left"),
+    research_text,
+    highlightPython,
+    document.getElementById("research-button"),
+    $("#research-fast-forward")
+);
 
-        // add the typewriter animation sequence to the "edu" block
-        scriptColumns(
-            $("#edu-button"),
-            document.getElementById("edu-script"),
-            $("#edu-right"),
-            $("#edu-left"),
-            edu_text,
-            highlightJulia,
-            document.getElementById('edu-button'),
-            $("#edu-fast-forward"),
-            function () {
-                setTimeout(
-                    function () {$("#advisor-error").hide(500)},
-                    8000
-                    );
-                }
-                );
+// add the typewriter animation sequence to the "edu" block
+scriptColumns(
+    $("#edu-button"),
+    document.getElementById("edu-script"),
+    $("#edu-right"),
+    $("#edu-left"),
+    edu_text,
+    highlightJulia,
+    document.getElementById("edu-button"),
+    $("#edu-fast-forward"),
+    function () {
+        setTimeout(function () {
+            $("#advisor-error").hide(500);
+        }, 8000);
+    }
+);
